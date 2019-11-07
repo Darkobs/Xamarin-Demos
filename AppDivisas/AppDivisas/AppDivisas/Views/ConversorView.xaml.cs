beaf8f59224626e2c336981f9be5af680595ad4e
Carlos Murillo <chaccool@hotmail.com>
2019-11-06 22:10:26 -0600

    ConversorViews event handlers

--- AppDivisas/AppDivisas/AppDivisas/Views/ConversorView.xaml.cs
+++ AppDivisas/AppDivisas/AppDivisas/Views/ConversorView.xaml.cs
@@ -12,19 +12,38 @@ namespace AppDivisas.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ConversorView : ContentPage
     {
+        const double tasa = 19.11;
+
         public ConversorView()
         {
             InitializeComponent();
         }
 
-        private void ConversorButton_Clicked(object sender, EventArgs e)
+        void Limpiar()
         {
+            PesosEntry.Text = string.Empty;
+            DolaresLabel.Text = string.Empty;
+        }
 
+        private async void ConversorButton_Clicked(object sender, EventArgs e)
+        {
+            double pesos = 0, dolares = 0;
+
+            if (double.TryParse(PesosEntry.Text, out pesos))
+            {
+                dolares = pesos / tasa;
+                DolaresLabel.Text = $"{dolares:N2}";
+            }
+            else
+            {
+                await DisplayAlert("Error", "Cantidad no válida", "OK");
+                Limpiar();
+            }
         }
 
         private void LimpiarButton_Clicked(object sender, EventArgs e)
         {
-
+            Limpiar();
         }
     }
 }
