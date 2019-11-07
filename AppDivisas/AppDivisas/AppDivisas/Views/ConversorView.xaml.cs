18710daf8a6e4574641f8e32c144d7e09ee280fa
Carlos Murillo <chaccool@hotmail.com>
2019-11-06 22:01:59 -0600

    ConversorView Content Page added

---
+++ AppDivisas/AppDivisas/AppDivisas/Views/ConversorView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppDivisas.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConversorView : ContentPage
+    {
+        public ConversorView()
+        {
+            InitializeComponent();
+        }
+
+        private void ConversorButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LimpiarButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
